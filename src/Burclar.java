36ca28fc3e6a3248a47b72597e38caea94d30980
yasar.bal <ysr.bal@gmail.com>
2022-06-27 22:35:55 +0300

    güncellemeler yapildi.

--- src/Burclar.java
+++ src/Burclar.java
@@ -3,111 +3,118 @@ import java.util.Scanner;
 public class Burclar {
     public static void main(String[] args) {
         int month, day;
+        String burc = "";
+        boolean isError = false;
         Scanner input = new Scanner(System.in);
 
         System.out.print("Dogdugunuz Gun : ");
-        day  = input.nextInt();
+        day = input.nextInt();
         System.out.print("Dogdugunuz Ay : ");
         month = input.nextInt();
 
         if (month == 1) {
             if (day < 22) {
-                System.out.println("Burcunuz : Oglak ");
+                burc = "Oglak";
             } else if (day > 31) {
-                System.out.println("Hatali Gun Girdiniz.");
+                isError = true;
             } else {
-                System.out.println("Burcunuz : Kova ");
+                burc = "Kova";
             }
         } else if ((month == 2)) {
             if (day < 20) {
-                System.out.println("Burcunuz : Kova ");
+                burc = "Kova";
             } else if (day > 28) {
-                System.out.println("Hatali Gun Girdiniz.");
+                isError = true;
             } else {
-                System.out.println("Burcunuz : Balik ");
+                burc = "Balik";
             }
         } else if (month == 3) {
             if (day < 21) {
-                System.out.println("Burcunuz : Balik");
+                burc = "Balik";
             } else if (day > 31) {
-                System.out.println("Hatali Gun Girdiniz.");
+                isError = true;
             } else {
-                System.out.println("Burcunuz : Koc");
+                burc = "Koc";
             }
         } else if (month == 4) {
             if (day < 21) {
-                System.out.println("Burcunuz : Koc");
+                burc = "Koc";
             } else if (day > 30) {
-                System.out.println("Hatali Gun Girdiniz.");
+                isError = true;
             } else {
-                System.out.println("Burcunuz : Boga");
+                burc = "Boga";
             }
         } else if (month == 5) {
             if (day < 22) {
-                System.out.println("Burcunuz : Boga");
+                burc = "Boga";
             } else if (day > 31) {
-                System.out.println("Hatali Gun Girdiniz.");
+                isError = true;
             } else {
-                System.out.println("Burcunuz : Ikizler");
+                burc = "Ikizler";
             }
         } else if (month == 6) {
             if (day < 23) {
-                System.out.println("Burcunuz : Ikizler");
+                burc = "Ikizler";
             } else if (day > 30) {
-                System.out.println("Hatali Gun Girdiniz.");
+                isError = true;
             } else {
-                System.out.println("Burcunuz : Yengec");
+                burc = "Yengec";
             }
         } else if (month == 7) {
             if (day < 23) {
-                System.out.println("Burcunuz : Yengec");
+                burc = "Yengec";
             } else if (day > 31) {
-                System.out.println("Hatali Gun Girdiniz.");
+                isError = true;
             } else {
-                System.out.println("Burcunuz : Aslan");
+                burc = "Aslan";
             }
         } else if (month == 8) {
             if (day < 23) {
-                System.out.println("Burcunuz : Aslan");
+                burc = "Aslan";
             } else if (day > 31) {
-                System.out.println("Hatali Gun Girdiniz.");
+                isError = true;
             } else {
-                System.out.println("Burcunuz : Basak");
+                burc = "Basak";
             }
         } else if (month == 9) {
             if (day < 23) {
-                System.out.println("Burcunuz : Basak");
+                burc = "Basak";
             } else if (day > 30) {
-                System.out.println("Hatali Gun Girdiniz.");
+                isError = true;
             } else {
-                System.out.println("Burcunuz : Terazi");
+                burc = "Terazi";
             }
         } else if (month == 10) {
             if (day < 23) {
-                System.out.println("Burcunuz : Terazi");
+                burc = "Terazi";
             } else if (day > 31) {
-                System.out.println("Hatali Gun Girdiniz.");
+                isError = true;
             } else {
-                System.out.println("Burcunuz : Akrep");
+                burc = "Akrep";
             }
         } else if (month == 11) {
             if (day < 22) {
-                System.out.println("Burcunuz : Akrep");
+                burc = "Akrep";
             } else if (day > 30) {
-                System.out.println("Hatali Gun Girdiniz.");
+                isError = true;
             } else {
-                System.out.println("Burcunuz : Yay");
+                burc = "Yay";
             }
         } else if (month == 12) {
             if (day < 22) {
-                System.out.println("Burcunuz : Yay");
+                burc = "Yay";
             } else if (day > 31) {
-                System.out.println("Hatali Gun Girdiniz.");
+                isError = true;
             } else {
-                System.out.println("Burcunuz : Oglak");
+                burc = "Oglak";
             }
-        }else {
+        } else {
             System.out.println("Hatali Ay Girisi Yaptiniz.");
+        }
+        if (isError) {
+            System.out.println("Hatali Gun Girdiniz.");
+        } else {
+            System.out.println("Burcunuz : " + burc);
         }
     }
 }
